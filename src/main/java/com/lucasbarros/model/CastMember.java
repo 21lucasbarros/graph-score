fb26ae08227f76a8c777db26f6874efa4a926c41
Lucas Barros Simon <lucasweacked21@gmail.com>
2026-07-19 05:12:07 -0300

    feat: criei um Enum para tipo de papel, se eh protagonista, coadjuvante ou apenas um cameo.

--- src/main/java/com/lucasbarros/model/CastMember.java
+++ src/main/java/com/lucasbarros/model/CastMember.java
@@ -2,4 +2,18 @@ package com.lucasbarros.model;
 
 public class CastMember {
     private String actorName;
+    private RoleType role;
+
+    public CastMember(String actorName, RoleType role) {
+        this.actorName = actorName;
+        this.role = role;
+    }
+
+    public String getActorName() {
+        return actorName;
+    }
+
+    public RoleType getRole() {
+        return role;
+    }
 }
